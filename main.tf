965f57448eab723afa0ec78163ac8ff6f9f534c6
ashraf <ashraf1abdelrasool@gmail.com>
2026-03-26 15:25:45 +0200

    use dynamic pod IDs and update module source branch

--- main.tf
+++ main.tf
@@ -95,7 +95,7 @@ module "store-core" {
 }
 
 module "store-pod" {
-  source           = "git::https://github.com/cvhome-saas/cvhome-store-pod.git?ref=feat/dynamic-pod"
+  source           = "git::https://github.com/cvhome-saas/cvhome-store-pod.git?ref=main"
   vpc_id           = module.vpc.vpc_id
   public_subnets   = module.vpc.public_subnets
   private_subnets  = module.vpc.private_subnets
@@ -118,7 +118,7 @@ module "store-pod" {
 }
 
 module "store-pod-n" {
-  source           = "git::https://github.com/cvhome-saas/cvhome-store-pod.git?ref=feat/dynamic-pod"
+  source           = "git::https://github.com/cvhome-saas/cvhome-store-pod.git?ref=main"
   vpc_id           = module.vpc.vpc_id
   public_subnets   = module.vpc.public_subnets
   private_subnets  = module.vpc.private_subnets
